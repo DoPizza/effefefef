bd83161fd8ecb9033aa1afd5d8bb795cecd4977f
DoPizza <153287400+DoPizza@users.noreply.github.com>
2026-09-10 00:01:01 +0400

    Create update.options.ts

---
+++ api/events/update.options.ts
@@ -0,0 +1,10 @@
+export default async function handler() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    }
+  });
+}
