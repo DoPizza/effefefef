38c380b84133e1be55049606fd27f242c228c780
DoPizza <153287400+DoPizza@users.noreply.github.com>
2026-09-09 23:34:33 +0400

    Create status.options.ts

---
+++ api/events/[id]/status.options.ts
@@ -0,0 +1,24 @@
+
+export default async () => {
+
+    return {
+
+        status: 204,
+
+        body: "",
+
+        headers: {
+
+            "Access-Control-Allow-Origin": "*",
+
+            "Access-Control-Allow-Methods":
+                "GET, PUT, PATCH, DELETE, OPTIONS",
+
+            "Access-Control-Allow-Headers":
+                "Content-Type",
+
+        },
+
+    };
+
+};
